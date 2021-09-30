fb83e4de90e72a11c55d7c3861604553891a19df
Mario Sanchez <mcn_09@live.com>
2021-09-30 15:42:28 -0600

    error axios no importado

--- src/views/Cliente/UserFeedback.jsx
+++ src/views/Cliente/UserFeedback.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Axios from "axios";
 import { Button } from "react-bootstrap";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
